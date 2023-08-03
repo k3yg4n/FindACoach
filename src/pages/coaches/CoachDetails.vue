9bfafd699d2c3273c6b0d0b36731c26ed87119a4
Keegan Liu <keeganliu5678@gmail.com>
2023-08-02 20:00:09 -0700

    Implement CoachDetails page

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,7 +1,68 @@
 <template>
-  Coach Details
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <p>{{ description }}</p>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button :link="true" :to="contactLink">Contact</base-button>
+      </header>
+      <!-- Contact form -->
+      <router-view></router-view>
+    </base-card>
+  </section>
+
+  <section>
+    <base-card>
+      <h2>Coachable Games:</h2>
+      <base-badge
+        v-for="game in games"
+        :key="game"
+        :title="game"
+        :type="game"
+      ></base-badge>
+    </base-card>
+  </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  // We receive an id as a prop because this component will
+  // be loaded through routing (CoachDetail) with props: true
+  // which forwards the dynamic value for the url as a prop
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  // Runs when the component is created
+  created() {
+    const coaches = this.$store.getters['coaches/coaches'];
+    this.selectedCoach = coaches.find((coach) => coach.id === this.id);
+  },
+  computed: {
+    fullName() {
+      return `${this.selectedCoach.firstName} ${this.selectedCoach.lastName}`;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    games() {
+      return this.selectedCoach.games;
+    },
+    contactLink() {
+      return `${this.$route.path}/${this.id}/contact`; // ex: /coaches/c1/contact
+    },
+  },
+};
+</script>
