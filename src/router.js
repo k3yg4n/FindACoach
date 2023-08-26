f90fd9d198f97d37f28a69b8c1d90b695f82b32d
Keegan Liu <keeganliu5678@gmail.com>
2023-08-25 22:10:04 -0700

    Add navigation guards to sensitive pages

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,11 +21,30 @@ const router = createRouter({
       props: true,
       children: [{ path: 'contact', component: ContactCoach }], // /coaches/:id/contact
     },
-    { path: '/register', component: CoachRegistration },
-    { path: '/requests', component: RequestsReceived },
-    { path: '/auth', component: UserAuth },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
+});
+
+// Global navigation guard (register, request, and auth routes)
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth'); // Redirect to /auth
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches'); // Redirect to /coaches
+  } else {
+    next(); // Otherwise continue as planned
+  }
 });
 
 export default router;
