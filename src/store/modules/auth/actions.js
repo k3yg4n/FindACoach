07f4b4a40ce165cf1ccce33c455a82efca273db0
Keegan Liu <keeganliu5678@gmail.com>
2023-08-25 22:32:08 -0700

    Add auto-login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,16 +1,23 @@
 export default {
-  async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBbAOtH_4TKYqfjMI_IV9xIpj8vEqvnDg4',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+  // Contains both logic for signup and login
+  async auth(context, payload) {
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBbAOtH_4TKYqfjMI_IV9xIpj8vEqvnDg4';
+
+    const mode = payload.mode;
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbAOtH_4TKYqfjMI_IV9xIpj8vEqvnDg4';
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = await response.json();
 
@@ -22,6 +29,10 @@ export default {
       throw error;
     } else {
       // Req Succeeded
+      localStorage.setItem('token', responseData.idToken); // Store values in local storage
+      localStorage.setItem('userId', responseData.localId);
+      localStorage.setItem('tokenExpiration', responseData.expiresIn);
+
       context.commit('setUser', {
         token: responseData.idToken,
         currentUserId: responseData.localId,
@@ -29,33 +40,29 @@ export default {
       });
     }
   },
+  async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBbAOtH_4TKYqfjMI_IV9xIpj8vEqvnDg4',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  // Dispatched when the app starts (App.vue file)
+  tryAutoLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
 
-    const responseData = await response.json();
-
-    // Req Failure
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message || 'Failed to authenticate.'
-      );
-      throw error;
-    } else {
-      // Req Succeeded
+    if (token && userId) {
       context.commit('setUser', {
-        token: responseData.idToken,
-        currentUserId: responseData.localId,
-        tokenExpiration: responseData.expiresIn,
+        token: token,
+        currentUserId: userId,
+        tokenExpiration: tokenExpiration,
       });
     }
   },
